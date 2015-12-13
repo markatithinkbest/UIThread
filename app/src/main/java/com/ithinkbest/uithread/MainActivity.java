6ca4009c49ece29e865504173e1148d1d2e18333
markatithinkbest <mark@ithinkbest.com>
2015-12-13 22:45:24 +0800

    WHY CRASH ONLY ON twibm003 case?

--- app/src/main/java/com/ithinkbest/uithread/MainActivity.java
+++ app/src/main/java/com/ithinkbest/uithread/MainActivity.java
@@ -6,10 +6,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.Random;
 //import java.util.logging.Handler;
@@ -17,8 +25,7 @@ import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-
-
+    RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         textView = (TextView) findViewById(R.id.textView);
-
+        queue = Volley.newRequestQueue(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +45,34 @@ public class MainActivity extends AppCompatActivity {
 
                 textView.setText("99");
                 //   randomize();
+
+//                String url ="http://www.google.com";
+//                String url ="http://twibm003.herokuapp.com/dummy"; // WHY NOT WORKING FOR THIS CASE?
+//              String url ="http://twibm003.herokuapp.com/dummy"; // WHY NOT WORKING FOR THIS CASE?
+//                http://twibm003.herokuapp.com/dummy
+
+//                String url =" http://stackoverflow.com/questions/20059576/import-android-volley-to-android-studio";
+
+                String url ="http://expressjs.com/en/api.html";
+                Log.d("mark123", "url is " + url);
+
+// Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+
+                                textView.setText("Response is: "+ response.substring(0,50));
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("That didn't work!");
+                    }
+                });
+// Add the request to the RequestQueue.
+                queue.add(stringRequest);
 
             }
         });
